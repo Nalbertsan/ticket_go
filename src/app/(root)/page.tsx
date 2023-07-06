f8385c9bd20a61d8f8d772531fd58ca5b78de56b
Nalbertsan <nalbert1000@gmail.com>
2023-07-05 22:34:43 -0300

    :confetti_ball: Criação da tela de Login

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,12 +4,13 @@
 'use client';
 
 import React from 'react';
+import { RiAddCircleFill } from 'react-icons/ri';
 import Button from '../components/Button';
 
 export default function PageLayout() {
   return (
     <main className='h-full w-screen'>
         <h1>Page</h1>
-        <Button label='Button' onClick={() => {}}/>
+        <Button label='Click' className='w-[80%]' icon={RiAddCircleFill} onClick={() => {}}/>
     </main>);
 }
